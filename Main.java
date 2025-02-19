8149e25447be2367f51aa121d1d7939fbb4aa87b
datboisus <donvu251106@gmail.com>
2025-02-19 10:21:13 -0800

    1

--- Main.java
+++ Main.java
@@ -9,17 +9,31 @@ public class Main
     String input = "";
 
     ArrayList<String> lists = new ArrayList<String>();
-    list.add("phone");
-    list.add("computer");
-    list.add("laptop");
-    list.add("television");
-    list.add("newspaper");
-    lists size(ArrayList);
-  
+    System.out.println("please enter words, enter STOP to stop")
 
     while (!input.equals("STOP"))
     {
-      System.out.println(lists);
+      System.out.println("In the loop. Enter a word");
+      input = sc.nextline();
+
+      lists.add(input);
+
+      System.out.println("The Arraylist is now " + lists); 
     }
+    
+    System.out.println("Exited the loop. ArrayList is now " + lists);
+    System.out.println("Removing the last words");
+    int LastIndex = words.size()-1;
+    words.remove(LastIndex);
+
+    System.out.println("Removed the last word. Arraylist is now " + lists);
+
+    System.out.println ("the soze pf th elist is " + words.size());
+    System.out.println("ArrayLists:" + words);
+    Starting 
+    
+
   }
 }
+
+  
